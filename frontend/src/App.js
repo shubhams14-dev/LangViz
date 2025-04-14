54192e39ecfabfd95ba2c240d3686e9b2d6e2cae
shubhams14-dev <shubhamsalunke1401@gmail.com>
2025-04-14 18:04:54 -0400

    Code change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [code, setCode] = useState('');
   const [lang, setLang] = useState('python');
   const [img, setImg] = useState('');
-  const backendURL = process.env.REACT_APP_BACK_URL;
+  const backendURL = process.env.REACT_APP_BACKEND_URL;
 
 
   const generate = async () => {
